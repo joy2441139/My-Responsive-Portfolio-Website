bbf642e8338833089fb7579966383bd090bad61a
Tanvir Ahmed Joy <tanvirahmedjoy2441139@gmail.com>
2023-03-27 23:12:52 +0600

    personal project section completed

--- js/script.js
+++ js/script.js
@@ -11,3 +11,32 @@ tabsContainer.addEventListener("click", (e)=>{
         aboutSection.querySelector(target).classList.add("active");
     };
 });
+
+/* Project details popups */
+document.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("view-project-btn")){
+        toggleProjectPopup();
+        document.querySelector(".pp-popup").scrollTo(0,0);
+        projectItemDetails(e.target.parentElement);
+    }
+});
+function toggleProjectPopup(){
+    document.querySelector(".pp-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click", toggleProjectPopup);
+
+/* Hide popup when click outside of it */
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("pp-inner")){
+        toggleProjectPopup();
+    };
+});
+
+function projectItemDetails(projectItem){
+    document.querySelector(".pp-thumbnail img").src = projectItem.querySelector(".pp-item-thumbnail img").src;
+    document.querySelector(".pp-header h3").innerHTML = projectItem.querySelector(".pp-item-title").innerHTML;
+    document.querySelector(".pp-body").innerHTML = projectItem.querySelector(".pp-item-details").innerHTML;
+
+}
